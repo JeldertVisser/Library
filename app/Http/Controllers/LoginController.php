b86c1d2e4583707f222f5a32f22eae97507ff7e3
Jeldert1999 <jl.visser@st.hanze.nl>
2024-07-12 14:22:30 +0200

    Added User Authentication

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use App\Http\Requests\StoreLoginRequest;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //hoi
+    public function verify(StoreLoginRequest $request): RedirectResponse{
+        $validated = $request->validated();
+
+        if (Auth::attempt($validated)){
+            return redirect('/home');
+        }
+        else{
+            return redirect('/login');
+        }
+    }
+    public function create(): View{
+        return view('home', ['msg'=>'Welkom op de home page!']);
+    }
+
+    
+
+    
+
+    
 }
